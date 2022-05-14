4cc41f95b17d16c4ed34fb2d967a264829d72f8b
Omar Kassem <omarnehadelkassemsayed.abelwahab@st.uskudar.edu.tr>
2022-05-14 13:08:26 +0300

    Basic char animation

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -6,10 +6,13 @@ public class playerMovement : MonoBehaviour
     public float sides = 38f;
     public float jumpVelocity = 100f;
     private BoxCollider2D boxCollider2D;
-    [SerializeField] private LayerMask platformsLayerMask; 
+    [SerializeField] private LayerMask platformsLayerMask;
+
+    private Animator playerAnim;
 
     private void Awake() 
     {
+        playerAnim = gameObject.GetComponent<Animator>();
         rb = transform.GetComponent<Rigidbody2D>();
         boxCollider2D = transform.GetComponent<BoxCollider2D>(); 
     }
@@ -26,15 +29,20 @@ public class playerMovement : MonoBehaviour
     {
         if (Input.GetKey("d"))
         {
+            playerAnim.SetBool("IsMoving", true);
+
             rb.velocity = new Vector2(sides, rb.velocity.y);
         }
         else if (Input.GetKey("a"))
         {
+            playerAnim.SetBool("IsMoving", true);
             rb.velocity = new Vector2(-sides, rb.velocity.y);
         }
         else
         {
             rb.velocity = new Vector2(0, rb.velocity.y);
+            playerAnim.SetBool("IsMoving", false);
+
         }
     }
 
