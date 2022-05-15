7442a8138dff98782601f4aab6a160ce2573cdda
Omar Kassem <omarnehadelkassemsayed.abelwahab@st.uskudar.edu.tr>
2022-05-15 03:13:46 +0300

    trythisone

--- Assets/Scripts/ScoreScript.cs
+++ Assets/Scripts/ScoreScript.cs
@@ -7,7 +7,7 @@ using TMPro;
 public class ScoreScript : MonoBehaviour
 {
     public TextMeshProUGUI MyscoreText;
-    private int ScoreNum;
+    private int ScoreNum = 0;
     // Start is called before the first frame update
     void Start()
     {
