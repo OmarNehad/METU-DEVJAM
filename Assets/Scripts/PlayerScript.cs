db3f2062d75d69df673ac9f0dc0dcfe7709563ca
GoXu0 <34915983+GoXu0@users.noreply.github.com>
2022-05-15 11:34:37 +0300

    koakfoadgokd

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -72,13 +72,8 @@ public class PlayerScript : MonoBehaviour
 
         if (isRendered && !gameObject.GetComponent<SpriteRenderer>().isVisible)
         {
-
-            //TODO : SHOW MENU
-<<<<<<< Updated upstream
             deathmenu.SetActive(true);
             
-=======
->>>>>>> Stashed changes
 
         }
 
